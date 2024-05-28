c83866587af106ca48e9734d89b4a8ba1b573107
dharana <dharana7723@gmail.com>
2024-05-28 17:27:28 +0900

    feat: gitignore and docker-compose , dags python operator added

---
+++ dags/default_dag.py
@@ -0,0 +1,33 @@
+from datetime import timedelta, datetime
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'PythonOperator',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 8, 12),
+    catchup=False,
+)
+
+
+def print_hello():
+    return "hello!"
+
+
+hello_operator = PythonOperator(
+    task_id="say_hello",
+    python_callable=print_hello,
+    dag=dag,
+)
